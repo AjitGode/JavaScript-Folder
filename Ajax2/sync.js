a4a8bfd55bbd7cf466c7dd2dcd9b39bada4ddec8
ajit2599 <godeajit71@gmail.com>
2022-10-28 15:19:36 +0530

    sync

---
+++ Ajax2/sync.js
@@ -0,0 +1,88 @@
+// sync --- Blocking in nature 
+
+function A(){
+    console.log("A")
+}
+
+function B(){
+    console.log("B")
+}
+
+// B()
+// A()
+
+
+// async -- Non Blocking in nature
+
+function C(){
+   console.log("C")
+}
+
+function D(){
+    setTimeout(() => {
+        console.log("D")
+    },3000);
+}
+// D()
+// C()
+
+
+
+
+// function Facebook(){
+// setTimeout(() => {
+//        console.log("User Created") 
+// }, 3000);
+
+// setTimeout(() => {
+//     console.log("LogIn via Id")
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log("Home page")
+// }, 1000);
+// }
+
+// Facebook()
+
+
+// call back hell
+
+function Facebook(){
+    setTimeout(() => {
+        console.log("User Created")
+        setTimeout(() => {
+            console.log("LogIn Via Id")
+            setTimeout(() => {
+                console.log("Home Page")
+            }, 1000);
+        }, 2000);
+        
+    }, 3000);
+}
+
+// Facebook()
+
+
+//Promises -- Pending , resolve , reject 
+
+
+let Pro = new Promise(function(resolev,reject){
+    let a = 10
+    let b= 10
+
+    if(a==b){
+        resolev("Hii")
+    }
+    else{
+        reject("Bye")
+    }
+})
+
+// consuming the promises 
+
+Pro.then(function(str){
+    console.log(str)
+},function(str){
+    console.log(str)
+})
